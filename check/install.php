970587fd3aef1efdf9dac4222f61618ab30da399
Leo Feyer <github@contao.org>
2012-08-30 14:51:01 +0200

    Rewrote the web installer based on the pull request

--- check/install.php
+++ check/install.php
@@ -2,9 +2,9 @@
 
 /**
  * Contao Open Source CMS
- *
+ * 
  * Copyright (C) 2005-2012 Leo Feyer
- *
+ * 
  * @package Check
  * @link    http://contao.org
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
@@ -13,326 +13,270 @@
 require 'bootstrap.php';
 
 
-class Install
+/**
+ * Download a Contao .zip archive and extract it
+ * 
+ * @package   Check
+ * @author    Leo Feyer <https://github.com/leofeyer>
+ * @copyright Leo Feyer 2012
+ */
+class Installer
 {
+
 	/**
-	 * The function to execute shell commands
+	 * Shell function
 	 * @var string
 	 */
-	protected $strShellFunction;
+	protected $shell;
 
 	/**
-	 * The tool to fetch data on the system shell
+	 * Shell download command
 	 * @var string
 	 */
-	protected $strShellFetchCommand;
+	protected $download;
 
 	/**
-	 * The tool to unzip tata on the system shell
+	 * Shell unzip command
 	 * @var string
 	 */
-	protected $strShellUnzipCommand;
+	protected $unzip;
 
 	/**
-	 * Use php instead of the shell commands
-	 * @var bool
-	 */
-	protected $blnUsePHPOverShell=false;
-
-	/**
-	 * Extract the contao installation in witch directory
+	 * Target directory
 	 * @var string
 	 */
-	protected $strExtractTo;
+	protected $target;
 
 	/**
-	 * The version witch shpould be installed
+	 * Target version
 	 * @var string
 	 */
-	protected $strVersion;
+	protected $version;
+
+	/**
+	 * PHP requirements met
+	 * @var boolean
+	 */
+	protected $php = false;
+
+	/**
+	 * Installer availability
+	 * @var boolean
+	 */
+	protected $available = true;
 
 
 	/**
-	 * Initialize the class and so some kickstart work
-	 *
-	 * @param	void
-	 * @return	void
+	 * Check the requirements and start the installation
 	 */
-	public function init()
+	public function run()
 	{
-		// do some environment checks
-		$this->checkShellCommands();
-		$this->checkShellTools();
-		$this->checkPhpExtensions();
-
-		// set some runtime params
-		$this->strExtractTo = filter_var($_GET['path'], FILTER_SANITIZE_STRING);
-		$this->strVersion = filter_var($_GET['version'], FILTER_SANITIZE_STRING);
-
-		// start the download
-		$this->startDownload();
+		if (!$this->canUseShell() && !$this->canUsePhp()) {
+			$this->available = false;
+		} else {
+			$this->install();
+		}	
 	}
 
 
 	/**
-	 * Check if the download is possible
-	 *
-	 * @param	void
-	 * @return	bool	Return true if the download is possible, otherwize false
+	 * Check whether the shell can be used to install Contao
+	 * 
+	 * @return boolean True if the shell can be used to install Contao
 	 */
-	public function installPossible()
+	protected function canUseShell()
 	{
-		if ($this->strShellFetchCommand != '' || $this->blnUsePHPOverShell === true)
-		{
-			// download possible because we have the shell or we have php
-			return true;
+		// Check for shell_exec() or exec()
+		if (function_exists('shell_exec')) {
+			$this->shell = 'shell_exec';
+		} elseif (function_exists('exec')) {
+			$this->shell = 'exec';
 		}
 
-		return false;
+		// Return if we cannot access the shell
+		if ($this->shell == '') {
+			return false;
+		}
+
+		// Check for wget or curl
+		if ($this->exec('which wget') != '') {
+			$this->download = 'wget';
+		} elseif ($this->exec('which curl') != '') {
+			$this->download = 'curl';
+		}
+
+		// Return if we cannot download on the shell
+		if ($this->download == '') {
+			return false;
+		}
+
+		// Check for unzip
+		if ($this->exec('which unzip') != '') {
+			$this->unzip = 'unzip';
+		}
+
+		// Return if we cannot unzip on the shell
+		if ($this->unzip == '') {
+			return false;
+		}
+
+		return true;
 	}
 
 
 	/**
-	 * Return an array with all available contao versions.
-	 * The informations are fetched from the github tags
-	 *
-	 * Example:
-	 * Array
-	 * (
-	 *    [3.0.beta1] => https://github.com/contao/core/zipball/3.0.beta1
-	 *    [3.0.RC1] => https://github.com/contao/core/zipball/3.0.RC1
-	 * )
-	 *
-	 * @param	void
-	 * @return	array	Return an array witch all tags
+	 * Check whether PHP can be used to install Contao
+	 * 
+	 * @return boolean True if PHP can be used to install Contao
 	 */
-	public function getTags()
+	protected function canUsePhp()
 	{
-		$arrTags = array();
-		$arrResponse = array();
-		$strJson = '';
-		$strCommand = '';
-
-		// get the tags via the php internal functions
-		if ($this->blnUsePHPOverShell === true)
-		{
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_HEADER, false);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_URL, 'https://api.github.com/repos/contao/core/tags');
-			$strJson =  curl_exec($ch);
-			curl_close($ch);
+		// Check whether cURL and Zip are available
+		if (!extension_loaded('curl') || !extension_loaded('zip')) {
+			return false;
 		}
 
-
-		// get the tags with the server shell
-		if ($this->blnUsePHPOverShell === false)
-		{
-			// use curl for the download
-			if ($this->strShellFetchCommand === 'curl')
-			{
-				$strCommand = 'curl https://api.github.com/repos/contao/core/tags';
-			}
-
-			// use wget for the download
-			if ($this->strShellFetchCommand === 'wget')
-			{
-				$strCommand = 'wget -qO- https://api.github.com/repos/contao/core/tags';
-			}
-
-			$strJson = $this->executeShellCommand($stCommand);
+		// Try to write a file
+		if (@file_put_contents('download', '') === false) {
+			return false;
 		}
 
+		$this->php = true;
+		@unlink('download');
 
-		// prepare the json string and build the array
-		$arrResponse = json_decode($strJson);
-
-		// get every tag name
-		foreach ($arrResponse as $v)
-		{
-			$arrTags[$v->name] = $v->zipball_url;
-		}
-
-		// put all new tags on the top of the list
-		arsort($arrTags);
-
-		return $arrTags;
+		return true;
 	}
 
 
 	/**
-	 * Start the installation if the url para "download" is set
-	 *
-	 * @param	void
-	 * @return	void
+	 * Execute a shell command and trim the result
+	 * 
+	 * @param string $command The shell command
+	 * 
+	 * @return string The trimmed output string
 	 */
-	protected function startDownload()
+	protected function exec($command)
 	{
-		if ($_GET['download'] == 'do')
-		{
-			// get all tags and the zibball_url from it
-			$arrTagsLookup = $this->getTags();
-
-
-			// start the download with php
-			if ($this->blnUsePHPOverShell === true)
-			{
-				$this->downloadWithPhp($arrTagsLookup[$this->strVersion]);
-			}
-			// start the download with the shell tools
-			else
-			{
-				$this->downloadWithShell($arrTagsLookup[$this->strVersion]);
-			}
-		}
+		$function = $this->shell;
+		return trim($function($command));
 	}
 
 
 	/**
-	 * Download the contao package with the internal PHP functions
-	 *
-	 * @param	string	$strUrl
-	 * @return	void
+	 * Retrieve information using cURL
+	 * 
+	 * @param string $url The URL
+	 * 
+	 * @return string The output string
 	 */
-	protected function downloadWithPhp($strUrl)
+	protected function curl($url)
 	{
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_HEADER, false);
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_URL, $strUrl);
-		file_put_contents('download', curl_exec($ch));
+		curl_setopt($ch, CURLOPT_URL, $url);
+		$return = curl_exec($ch);
 		curl_close($ch);
 
-		$zip = new ZipArchive;
-		$zip->open('download');
-		$zip->extractTo($this->strExtractTo);
-		$zip->close();
-
-		unlink('download');
+		return $return;
 	}
 
 
 	/**
-	 * Download the contao package with the server shell
-	 *
-	 * @param	string	$strUrl
-	 * @return	void
+	 * Check whether the automatic installation is possible
+	 * 
+	 * @return boolean True if the automatic installation is possible
 	 */
-	protected function downloadWithShell($strUrl)
+	public function available()
 	{
-		// build the download command witch wget
-		if ($this->strShellFetchCommand == 'wget')
-		{
-			$strCommand = 'wget ' . $strUrl;
+		return $this->available;
+	}
+
+
+	/**
+	 * Return all tagged Contao versions as array
+	 * 
+	 * @return array The tags array
+	 */
+	public function tags()
+	{
+		// Download the JSON file
+		if ($this->php === false) {
+			if ($this->download === 'curl') {
+				$json = $this->exec('curl https://api.github.com/repos/contao/core/tags');
+			} elseif ($this->download === 'wget') {
+				$json = $this->exec('wget -qO- https://api.github.com/repos/contao/core/tags');
+			}
+		} else {
+			$json = $this->curl('https://api.github.com/repos/contao/core/tags');
 		}
 
-		// vuild the download command witch curl
-		if ($this->strShellFetchCommand == 'curl')
-		{
-			$strCommand = 'curl -s -L ' . $strUrl . ' > download';
+		$tags = array();
+
+		// Prepare the return array
+		foreach (json_decode($json) as $tag) {
+			if (preg_match('/^[0-9\.]+$/', $tag->name)) {
+				$tags[] = $tag->name;
+			}
 		}
 
-		// download, unzip and cleanup
-		$this->executeShellCommand($strCommand);
-		$this->executeShellCommand($this->strShellUnzipCommand . ' download');
-		$this->executeShellCommand('rm download');
+		natsort($tags);
+		return array_reverse($tags);
 	}
 
 
 	/**
-	 * Execute the given command on the system shell
-	 *
-	 * @param	string	$strCommand		The command witch should be executed
-	 * @return	string					The output from the shell command
+	 * Start the installation
 	 */
-	protected function executeShellCommand($strCommand)
+	protected function install()
 	{
-		$strFunction = $this->strShellFunction;
-		return $strFunction($strCommand);
-	}
-
-
-	/**
-	 * Check witch method should be used to run commands on the system shell
-	 *
-	 * @param	void
-	 * @return	void
-	 */
-	private function checkShellCommands()
-	{
-		// check if shell_exec is available
-		if (function_exists('shell_exec'))
-		{
-			$this->strShellFunction = 'shell_exec';
+		if (!isset($_POST['version'])) {
 			return;
 		}
 
-		// check if exec is available
-		if (function_exists('exec'))
-		{
-			$this->strShellFunction = 'exec';
+		$target = dirname(__DIR__);
+
+		if (!is_writable($target)) {
 			return;
 		}
-	}
 
+		$version = filter_var($_POST['version'], FILTER_SANITIZE_STRING);
+		$url = 'https://github.com/contao/core/zipball/' . $version;
 
-	/**
-	 * Check witch tools are available on the system shell
-	 *
-	 * @param	void
-	 * @return	void
-	 */
-	private function checkShellTools()
-	{
-		// check if wget is available
-		if ($this->executeShellCommand('which wget') != '')
-		{
-			$this->strShellFetchCommand = 'wget';
-		}
+		if ($this->php === false) {
+			if ($this->download == 'wget') {
+				$this->exec("wget $url");
+			} elseif ($this->download == 'curl') {
+				$this->exec("curl -s -L $url > download");
+			}
 
-		// check if curl is available and override wget, because curl is better
-		if ($this->executeShellCommand('which curl') != '')
-		{
-			$this->strShellFetchCommand = 'curl';
-		}
+			// Extract
+			if (file_exists('download')) {
+				$this->exec($this->unzip . ' download');			
+				$this->exec('rm download');
+				$folder = $this->exec('ls -d contao-*');
+				$this->exec("mv $folder/* ../");
+				$this->exec("rm -rf $folder");
+			}
+		} else {
+			file_put_contents('download', $this->curl($url));
 
-
-		// check if unzip is available
-		if ($this->executeShellCommand('which unzip') != '')
-		{
-			$this->strShellUnzipCommand = 'unzip';
-		}
-
-		//TODO: add support for "tar -xf"
-	}
-
-
-	/**
-	 * Check if all needed PHP functions are available
-	 *
-	 * @param	void
-	 * @return	void
-	 */
-	private function checkPhpExtensions()
-	{
-		// check if curl and zip are available
-		if (extension_loaded('curl') === true && extension_loaded('zip') === true)
-		{
-			// extensions are available, now try the writing to the disk
-			if (file_put_contents('write-test', 'Chuck Norris can always write to the disk, can your PHP to?') != false)
-			{
-				$this->blnUsePHPOverShell = true;
-				@unlink('write-test');
+			// Extract
+			if (file_exists('download')) {
+				$zip = new ZipArchive;
+				$zip->open('download');
+				$zip->extractTo($target);
+				$zip->close();
+				unlink('download');
 			}
 		}
 	}
 }
 
 
-$objInstall = new Install();
-$objInstall->init();
+$installer = new Installer;
+$installer->run();
 
 
 ?>
@@ -350,43 +294,36 @@ $objInstall->init();
   </div>
   <div class="row">
     <h2><?php echo _('Web installer') ?></h2>
-    <?php if ($objInstall->installPossible()): ?>
-      <p class="confirm"><?php printf(_('Will using the fastest method to download and install contao on your server'), $exec) ?></p>
+    <?php if ($installer->available()): ?>
+      <p class="confirm"><?php echo _('The automatic installation is possible on your server.') ?></p>
     <?php else: ?>
       <p class="error"><?php echo _('The automatic installation is not possible on your server.') ?></p>
       <p class="explain"><?php echo _('Your PHP installation does not meet the requirements to use the command line, does not have enough permissions to create files and folders or does not have the required PHP extensions "cURL" and "Zip".') ?></p>
     <?php endif; ?>
   </div>
   <div class="row">
-    <?php if ($objInstall->installPossible() === false): ?>
+    <?php if (!$installer->available()): ?>
       <h2><?php echo _('Manual installation') ?></h2>
       <ul>
         <li><?php printf('Go to %s and download the latest Contao version.', '<a href="http://sourceforge.net/projects/contao/files/">sourceforge.net</a>') ?></li>
         <li><?php echo _('Extract the download archive and upload the files to your server using an (S)FTP client.') ?></li>
         <li><?php echo _('Open the Contao install tool by adding "/contao" to the URL of your installation.') ?></li>
       </ul>
-    <?php elseif (!isset($_GET['download'])): ?>
-    	<form action="install.php" method="get">
-    		<input type="text" name="path" value="." />
-    		<select name="version">
-
-    			<?php
-
-				foreach ($objInstall->getTags() as $k=>$v)
-				{
-					echo '<option value="' . $k . '">' . $k . '</option>';
-				}
-
-				?>
-
-    		</select>
-    		<input type="hidden" name="download" value="do" />
-    		<input class="btn" type="submit" title="<?php echo _('Start the installation') ?>" value="<?php echo _('Start the installation') ?>" />
-    	</form>
+    <?php elseif (!isset($_POST['version'])): ?>
+      <form method="post">
+        <select name="version">
+        <?php
+          foreach ($installer->tags() as $tag) {
+            echo '<option value="' . $tag . '">' . $tag . '</option>';
+          }
+        ?>
+        </select>
+        <input class="btn" type="submit" value="<?php echo _('Start the installation') ?>" />
+      </form>
     <?php else: ?>
       <h2><?php echo _('Installation complete') ?></h2>
-      <p class="confirm"><?php printf(_('Contao has been installed in %s/%s.'), __DIR__, $version) ?></p>
-      <p class="mt"><a href="<?php echo $version ?>/contao/install.php" class="btn"><?php echo _('Open the Contao install tool') ?></a></p>
+      <p class="confirm"><?php printf(_('Contao has been installed in %s.'), dirname(__DIR__)) ?></p>
+      <p class="mt"><a href="../contao/install.php" class="btn"><?php echo _('Open the Contao install tool') ?></a></p>
     <?php endif; ?>
   </div>
   <p class="back"><a href="."><?php echo _('Go back') ?></a></p>
