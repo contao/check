8faa2948d70a44e0b08f18b8fe4cf7a65ee7de1a
Leo Feyer <github@contao.org>
2013-01-08 13:03:07 +0100

    Added the Contao 2.11.8, 3.0.2 and 3.0.3 file hashes

--- check/controller/bootstrap.php
+++ check/controller/bootstrap.php
@@ -30,7 +30,7 @@ class Bootstrap
 
 		session_start();
 
-		define('CONTAO_CHECK_VERSION', '5.2');
+		define('CONTAO_CHECK_VERSION', '5.3');
 		define('IS_WINDOWS', (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN'));
 		define('TL_ROOT', dirname(dirname(dirname(__FILE__))));
 
