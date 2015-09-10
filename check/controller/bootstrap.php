8d5f69bbdf724351e27f0e18e48f3e4970801181
Leo Feyer <github@contao.org>
2015-09-10 10:47:17 +0200

    Add the version 3.5.3 file hashes.

--- check/controller/bootstrap.php
+++ check/controller/bootstrap.php
@@ -26,9 +26,9 @@ class Bootstrap
 
 		session_start();
 
-		define('CONTAO_CHECK_VERSION', '10.7');
-		define('CURRENT_VERSION', '3.5.2');
-		define('CURRENT_LTS_VERSION', '3.5.2');
+		define('CONTAO_CHECK_VERSION', '10.8');
+		define('CURRENT_VERSION', '3.5.3');
+		define('CURRENT_LTS_VERSION', '3.5.3');
 		define('IS_WINDOWS', (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN'));
 
 		$this->setLocale($this->getLanguage());
