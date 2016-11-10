2dc9c86d2e67fe1c85391508f7b86f7c31c8f26d
Leo Feyer <github@contao.org>
2016-11-10 16:58:28 +0100

    Download the version files on demand.

--- check/controller/installer.php
+++ check/controller/installer.php
@@ -137,104 +137,6 @@ class Installer
 	}
 
 	/**
-	 * Retrieve information using cURL
-	 *
-	 * @param string $url The URL
-	 *
-	 * @return string The output string
-     *
-     * @throws RuntimeException If the download fails
-	 */
-	protected function curl($url)
-	{
-		$ch = curl_init();
-
-		curl_setopt($ch, CURLOPT_HEADER, false);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-		curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux i586; rv:31.0) Gecko/20100101 Firefox/31.0');
-
-		if (($met = ini_get('max_execution_time')) > 0) {
-			curl_setopt($ch, CURLOPT_TIMEOUT, round($met * 0.9));
-		}
-
-		// cURL will follow redirects if open_basedir is not set
-		if (ini_get('open_basedir') == '') {
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-			curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
-			$return = curl_exec($ch);
-
-			if (curl_errno($ch)) {
-				$error = curl_error($ch);
-				curl_close($ch);
-
-				throw new RuntimeException($error);
-			}
-
-			curl_close($ch);
-
-			return $return;
-		}
-
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
-
-		$new = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
-		$rch = curl_copy_handle($ch);
-
-		curl_setopt($rch, CURLOPT_HEADER, true);
-		curl_setopt($rch, CURLOPT_NOBODY, true);
-		curl_setopt($rch, CURLOPT_FORBID_REUSE, false);
-		curl_setopt($rch, CURLOPT_RETURNTRANSFER, true);
-
-		$max = 10;
-
-		do {
-			curl_setopt($rch, CURLOPT_URL, $new);
-			$header = curl_exec($rch);
-
-			if (curl_errno($rch)) {
-				$error = curl_error($rch);
-				curl_close($rch);
-
-				throw new RuntimeException($error);
-			}
-
-			$code = curl_getinfo($rch, CURLINFO_HTTP_CODE);
-
-			if ($code == 301 || $code == 302) {
-				$matches = array();
-				preg_match('/Location:(.*?)\n/', $header, $matches);
-				$new = trim(array_pop($matches));
-			} else {
-				$code = 0;
-			}
-		} while ($code && --$max);
-
-		curl_close($rch);
-
-		if ($max > 0) {
-			curl_setopt($ch, CURLOPT_URL, $new);
-			$return = curl_exec($ch);
-
-			if (curl_errno($ch)) {
-				$error = curl_error($ch);
-				curl_close($ch);
-
-				throw new RuntimeException($error);
-			}
-		} else {
-			curl_close($ch);
-
-			throw new RuntimeException('Too many redirects');
-		}
-
-		curl_close($ch);
-
-		return $return;
-	}
-
-	/**
 	 * Check whether the automatic installation is possible
 	 *
 	 * @return boolean True if the automatic installation is possible
@@ -275,29 +177,6 @@ class Installer
 	}
 
 	/**
-	 * Return the available version numbers
-	 *
-	 * @return array The versions array
-	 */
-	public function getVersions()
-	{
-		$versions = array();
-		$stable = CURRENT_VERSION;
-
-		if (file_exists(__DIR__ . "/../versions/$stable.json")) {
-			$versions[$stable] = $stable;
-		}
-
-		$lts = CURRENT_LTS_VERSION;
-
-		if (file_exists(__DIR__ . "/../versions/$lts.json")) {
-			$versions[$lts] = "$lts (LTS)";
-		}
-
-		return $versions;
-	}
-
-	/**
 	 * Start the installation
 	 *
 	 * @throws RuntimeException In case the version number is invalid
@@ -311,7 +190,7 @@ class Installer
 		$version = filter_var($_POST['version'], FILTER_SANITIZE_STRING);
 
 		// Validate the version number
-		if (!preg_match('/^[23](\.[0-9]{1,2}){2}$/', $version) || !file_exists("versions/$version.json")) {
+		if (!preg_match('/^[23](\.[0-9]{1,2}){2}$/', $version)) {
 			throw new RuntimeException("Invalid version number $version");
 		}
 
@@ -319,7 +198,7 @@ class Installer
 		$prefix = version_compare($version, '3.3.0', '>=') ? 'contao' : 'core';
 
 		try {
-			file_put_contents('download', $this->curl($url));
+			file_put_contents('download', curl($url));
 
 			// Extract
 			if (file_exists('download') && filesize('download') > 0) {
--- check/includes/util.php
+++ check/includes/util.php
@@ -38,3 +38,111 @@ function function_disabled($func)
 {
 	return in_array($func, array_map('trim', explode(',', ini_get('disable_functions'))));
 }
+
+/**
+ * Retrieve information using cURL
+ *
+ * @param string $url The URL
+ *
+ * @return string The output string
+ *
+ * @throws RuntimeException If the download fails
+ */
+function curl($url)
+{
+	$ch = curl_init();
+
+	curl_setopt($ch, CURLOPT_HEADER, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux i586; rv:31.0) Gecko/20100101 Firefox/31.0');
+
+	if (($met = ini_get('max_execution_time')) > 0) {
+		curl_setopt($ch, CURLOPT_TIMEOUT, round($met * 0.9));
+	}
+
+	// cURL will follow redirects if open_basedir is not set
+	if (false && ini_get('open_basedir') == '') {
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+		$return = curl_exec($ch);
+
+		if (curl_errno($ch)) {
+			$error = curl_error($ch);
+			curl_close($ch);
+
+			throw new RuntimeException($error);
+		}
+
+		$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		curl_close($ch);
+
+		if ($code != 200) {
+			throw new RuntimeException('The installation package could not be downloaded');
+		}
+
+		return $return;
+	}
+
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+
+	$new = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
+	$rch = curl_copy_handle($ch);
+
+	curl_setopt($rch, CURLOPT_HEADER, true);
+	curl_setopt($rch, CURLOPT_NOBODY, true);
+	curl_setopt($rch, CURLOPT_FORBID_REUSE, false);
+	curl_setopt($rch, CURLOPT_RETURNTRANSFER, true);
+
+	$max = 10;
+
+	do {
+		curl_setopt($rch, CURLOPT_URL, $new);
+		$header = curl_exec($rch);
+
+		if (curl_errno($rch)) {
+			$error = curl_error($rch);
+			curl_close($rch);
+
+			throw new RuntimeException($error);
+		}
+
+		$code = curl_getinfo($rch, CURLINFO_HTTP_CODE);
+
+		if ($code == 301 || $code == 302) {
+			$matches = array();
+			preg_match('/Location:(.*?)\n/', $header, $matches);
+			$new = trim(array_pop($matches));
+		} else {
+			$code = 0;
+		}
+	} while ($code && --$max);
+
+	curl_close($rch);
+
+	if ($max > 0) {
+		curl_setopt($ch, CURLOPT_URL, $new);
+		$return = curl_exec($ch);
+
+		if (curl_errno($ch)) {
+			$error = curl_error($ch);
+			curl_close($ch);
+
+			throw new RuntimeException($error);
+		}
+	} else {
+		curl_close($ch);
+
+		throw new RuntimeException('Too many redirects');
+	}
+
+	$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	curl_close($ch);
+
+	if ($code != 200) {
+		throw new RuntimeException('The installation package could not be downloaded');
+	}
+
+	return $return;
+}
